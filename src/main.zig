34f24e8910df53f1892490e299292670a312e176
Lordink <infernalstudios@gmail.com>
2025-07-21 23:21:23 +0700

    Drawing single dot to a chosen point with Renderer

--- src/main.zig
+++ src/main.zig
@@ -11,12 +11,19 @@ pub fn main() !void {
     try sdl3.init(init_flags);
     defer sdl3.quit(init_flags);
 
-    const window = try sdl3.video.Window.init("ZSR", SCREEN_W, SCREEN_H, .{});
+    const result = try sdl3.render.Renderer.initWithWindow("ZSR", SCREEN_W, SCREEN_H, .{});
+    const window = result.window;
+    const renderer = result.renderer;
+    // const window = try sdl3.video.Window.init("ZSR", SCREEN_W, SCREEN_H, .{});
     defer window.deinit();
+    defer renderer.deinit();
 
     const surface = try window.getSurface();
-    try surface.fillRect(null, surface.mapRgb(120, 91, 191));
+    try surface.fillRect(null, surface.mapRgb(0, 0, 0));
     try window.updateSurface();
+    try renderer.setDrawColor(.{ .r = 200, .g = 100, .b = 51, .a = 255 });
+    try renderer.renderPoint(sdl3.rect.FPoint{ .x = 500, .y = 250 });
+    try renderer.present();
 
     while (true) {
         switch (try sdl3.events.waitAndPop()) {
