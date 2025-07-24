bc4f23eda4fa5ee225c9a4d9061a146247cdc4e4
Lordink <infernalstudios@gmail.com>
2025-07-25 00:02:19 +0700

    Got rid of GPU drawing methods

--- src/main.zig
+++ src/main.zig
@@ -11,19 +11,13 @@ pub fn main() !void {
     try sdl3.init(init_flags);
     defer sdl3.quit(init_flags);
 
-    const result = try sdl3.render.Renderer.initWithWindow("ZSR", SCREEN_W, SCREEN_H, .{});
-    const window = result.window;
-    const renderer = result.renderer;
-    // const window = try sdl3.video.Window.init("ZSR", SCREEN_W, SCREEN_H, .{});
+    const window = try sdl3.video.Window.init("ZSR", SCREEN_W, SCREEN_H, .{});
     defer window.deinit();
-    defer renderer.deinit();
 
     const surface = try window.getSurface();
     try surface.fillRect(null, surface.mapRgb(0, 0, 0));
+    try surface.writePixel(300, 200, .{ .r = 250, .g = 200, .b = 34, .a = 255 });
     try window.updateSurface();
-    try renderer.setDrawColor(.{ .r = 200, .g = 100, .b = 51, .a = 255 });
-    try renderer.renderPoint(sdl3.rect.FPoint{ .x = 500, .y = 250 });
-    try renderer.present();
 
     while (true) {
         switch (try sdl3.events.waitAndPop()) {
