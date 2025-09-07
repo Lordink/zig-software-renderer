96840d06df8e2164278349860f3b025d00992ca6
Lordink <infernalstudios@gmail.com>
2025-09-07 12:19:33 +0700

    Cleanup & reworked viewport coords into 1x1 with aspect ratio adjustment

--- src/main.zig
+++ src/main.zig
@@ -6,41 +6,61 @@ const print = std.debug.print;
 const SCREEN_W = 1280;
 const SCREEN_H = 720;
 
+const ASPECT_RATIO: f32 = @as(f32, @floatFromInt(SCREEN_W)) / @as(f32, @floatFromInt(SCREEN_H));
+
+// Whole viewport is just (1 * aspect_ratio)x1, for simplicity
+// World units
+const VIEWPORT_W = 1.0 * ASPECT_RATIO;
+const VIEWPORT_H = 1.0;
+// Distance from the camera to the viewport
+// World units
+const VIEWPORT_D = 1.0;
+
 const DEFAULT_COLOR: Color = .{ .r = 0, .g = 0, .b = 0, .a = 255 };
 
-const CANVAS_Z = 1.0;
-
 const Canvas = struct {
-    pub const width: i32 = 720;
-    pub const height: i32 = 405;
+    pub const width: i32 = SCREEN_W;
+    pub const height: i32 = SCREEN_H;
     pub fn toViewport(x: i32, y: i32) Vec3 {
         return .{
-            .x = @floatFromInt(@divFloor(x * SCREEN_W, width)),
-            .y = @floatFromInt(@divFloor(y * SCREEN_H, height)),
-            .z = CANVAS_Z,
+            .x = @as(f32, @floatFromInt(x)) * VIEWPORT_W / @as(f32, width),
+            .y = @as(f32, @floatFromInt(y)) * VIEWPORT_H / @as(f32, height),
+            .z = VIEWPORT_D,
         };
     }
 };
 
 // NOTE: Doesn't do window.updateSurface()
+// x and y are in Canvas Space
 fn paintPix(surf: *const sdl3.surface.Surface, x: i32, y: i32, col: sdl3.pixels.Color) !void {
+    // Canvas to screen (SDL) coords
     // Convert from center-is-at-0 to center-is-at-half-measure
-    const s_x: usize = @intCast(x + SCREEN_W / 2);
-    const s_y: usize = @intCast(y + SCREEN_H / 2);
-    try surf.writePixel(s_x, s_y, col);
+    const s_x = x + SCREEN_W / 2;
+    const s_y = SCREEN_H - (y + SCREEN_H / 2);
+
+    // may be off-screen
+    if (s_y < 0 or s_y >= SCREEN_H) {
+        return;
+    }
+    //print("{}\n", .{s_y});
+    try surf.writePixel(@intCast(s_x), @intCast(s_y), col);
 }
 const Vec3 = struct {
+    const Self = @This();
+
     x: f32,
     y: f32,
     z: f32,
 
-    pub fn dot(self: *const Vec3, other: Vec3) f32 {
+    // Dot product with another vec3
+    pub fn dot(self: *const Self, other: Vec3) f32 {
         return self.x * other.x + self.y * other.y + self.z * other.z;
     }
-    pub fn sub(self: *const Vec3, other: Vec3) Vec3 {
+    // subtract other vec from self
+    pub fn sub(self: *const Self, other: Vec3) Vec3 {
         return .{ .x = self.x - other.x, .y = self.y - other.y, .z = self.z - other.z };
     }
-    pub fn new(x: f32, y: f32, z: f32) Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) Self {
         return .{
             .x = x,
             .y = y,
@@ -49,9 +69,19 @@ const Vec3 = struct {
     }
 };
 
-const Sphere = struct { center: Vec3, radius: f32, col: Color };
+const Sphere = struct {
+    const Self = @This();
 
-fn rayTrace(spheres: []const Sphere, ray_origin: Vec3, ray_dir: Vec3, t_min: f32, t_max: f32) !Color {
+    center: Vec3,
+    radius: f32,
+    col: Color,
+
+    pub fn new(center: Vec3, radius: f32, color: Color) Self {
+        return .{ .center = center, .radius = radius, .col = color };
+    }
+};
+
+fn rayTraceSpheres(spheres: []const Sphere, ray_origin: Vec3, ray_dir: Vec3, t_min: f32, t_max: f32) !Color {
     var closest_t = std.math.floatMax(f32);
     var closest_sphere: ?*const Sphere = null;
 
@@ -112,11 +142,21 @@ pub fn main() !void {
     try window.updateSurface();
 
     const spheres = [_]Sphere{
-        Sphere{
-            .center = Vec3.new(0, -1.0, 3.0),
-            .radius = 100.0,
-            .col = .{ .r = 255, .g = 0, .b = 0, .a = 255 },
-        },
+        Sphere.new(
+            Vec3.new(0, -1.0, 3.0),
+            1.0,
+            .{ .r = 255, .g = 0, .b = 0, .a = 255 },
+        ),
+        Sphere.new(
+            Vec3.new(2.0, 0.0, 4.0),
+            1.0,
+            .{ .r = 0, .g = 0, .b = 255, .a = 255 },
+        ),
+        Sphere.new(
+            Vec3.new(-2.0, 0.0, 4.0),
+            1.0,
+            .{ .r = 0, .g = 255, .b = 0, .a = 255 },
+        ),
     };
 
     while (true) {
@@ -126,7 +166,7 @@ pub fn main() !void {
             var y: i32 = -Canvas.height / 2;
             while (y < Canvas.height / 2) {
                 const ray_dir = Canvas.toViewport(x, y);
-                const color = rayTrace(&spheres, o, ray_dir, 1.0, std.math.floatMax(f32)) catch DEFAULT_COLOR;
+                const color = rayTraceSpheres(&spheres, o, ray_dir, 1.0, std.math.floatMax(f32)) catch DEFAULT_COLOR;
                 try paintPix(&surface, x, y, color);
                 y += 1;
             }
@@ -144,11 +184,11 @@ pub fn main() !void {
 test "raySphereIntersect" {
     const expect = std.testing.expect;
 
-    const sphere: Sphere = .{
-        .center = Vec3.new(2.0, 0.0, 0.5),
-        .radius = 1.0,
-        .col = .{ .a = 255, .r = 255, .g = 255, .b = 255 },
-    };
+    const sphere = Sphere.new(
+        Vec3.new(2.0, 0.0, 0.5),
+        1.0,
+        .{ .a = 255, .r = 255, .g = 255, .b = 255 },
+    );
     const result = try raySphereIntersect(Vec3.new(0.0, 0.0, 0.0), Vec3.new(1.0, 0.0, 0.0), &sphere);
 
     print("Result: {}", .{result});
